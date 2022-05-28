5b5a7df46ee9e11652a69a4771df2fef17112576
Sho Emoto <dd2d00seg1@gmail.com>
2022-05-29 00:05:18 +0900

    FaceDetectionPage追加

---
+++ flutter_ar_make_up_sample/lib/pages/face_detection_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// カメラを起動して顔検出する画面
+class FaceDetectionPage extends StatefulWidget {
+  const FaceDetectionPage({Key? key}) : super(key: key);
+
+  @override
+  State<FaceDetectionPage> createState() => _FaceDetectionPageState();
+}
+
+class _FaceDetectionPageState extends State<FaceDetectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
