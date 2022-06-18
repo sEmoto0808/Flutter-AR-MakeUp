4e196d812e6c7314e943e48465c569630dd34ac2
Sho Emoto <dd2d00seg1@gmail.com>
2022-06-18 17:05:55 +0900

    CustomPainterを自作して円を描画する

---
+++ flutter_ar_make_up_sample/lib/pages/circle_paint_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// CustomPainterを自作して円を描画する
+class CirclePaintPage extends StatelessWidget {
+  const CirclePaintPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
